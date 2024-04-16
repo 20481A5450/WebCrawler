3a473472c11c637f41fe2fe77cfe0312602116e7
Zohaib Shaik <20481A5450@gecgudlavallerumic.in>
2024-04-16 14:04:38 +0530

    commited with some issues with the pipelines

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WebCrawler
+
+### This WebCrawler designed by scrapy where the web crawler, crawls through this website https://www.takeofftalent.com/ and some other and display's the results in here.
+
+| Position        | Company                | Location                 | Job type  | Job mode | Job requisition id | Years of experience | Job link |
+|-----------------|------------------------|---------------------------|-----------|----------|--------------------|---------------------|----------|
+| Data Scientist  | Micron Technology, Inc. | Hyderabad, Telangana, India | Full-time | On-site  | JR52043            | 0 years             | [Job link](https://example.com) |
--- webcrawler/webcrawler/pipelines.py
+++ webcrawler/webcrawler/pipelines.py
@@ -7,7 +7,26 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
-
 class WebcrawlerPipeline:
     def process_item(self, item, spider):
         return item
+
+class MarkdownPipeline:
+    def open_spider(self, spider):
+        self.file = open('C:/Users/shaik/Desktop/Projects Workspace/WebCrawler/README.md', 'a', encoding='utf-8')
+        # Write the table header if the file is new
+        self.file.seek(0, 2)  # Move to the end of the file
+        if self.file.tell() == 0:
+            self.file.write("| Position | Company | Location | Job Type | Job Mode | Job Requisition ID | Years of Experience | Job Link |\n")
+            self.file.write("| --- | --- | --- | --- | --- | --- | --- | --- |\n")
+
+    def process_item(self, item, spider):
+        # Log the item data for debugging
+        print(f"Processing item: {item}")
+        # Construct a Markdown table row from the item data
+        row = f"| {item.get('position', '')} | {item.get('company', '')} | {item.get('location', '')} | {item.get('job_type', '')} | {item.get('job_mode', '')} | {item.get('job_requisition_id', '')} | {item.get('years_of_experience', '')} | [Job Link]({item.get('job_link', '')}) |\n"
+        self.file.write(row)
+        return item
+
+    def close_spider(self, spider):
+        self.file.close()
